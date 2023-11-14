e857babe9e764621ac7d232f2c3cfd8b714c9fa8
KZhang2002 <kennethzhang2002@gmail.com>
2023-11-13 20:43:34 -0600

    Added syntax parser and error handling.

--- Code/FlowScriptInterpreter.cpp
+++ Code/FlowScriptInterpreter.cpp
@@ -7,11 +7,16 @@
 ostream &operator<<(ostream &os, const token &token) {
     os << "<" << tokenTypeNames[token.type];
     if (token.type == WORD) {
-        os << " | " << token.value;
+        os << " : " << token.value;
     }
     os  << ">";
     return os;
 }
+
+bool token::operator==(tokenType other) const {
+    return (type == other);
+}
+
 
 FlowScriptInterpreter::FlowScriptInterpreter(const string& relativePath) {
     ifstream input(relativePath);
@@ -21,71 +26,75 @@ FlowScriptInterpreter::FlowScriptInterpreter(const string& relativePath) {
 }
 
 void FlowScriptInterpreter::Interpret(ifstream &input) {
-    Lexer(input);
-    if(errorFlag) return;
+    l_Lexer(input);
+    if(e_Flag) return;
+	cout << endl;
     printLexResult();
+	cout << endl;
+
+	s_ParseSyntax();
 }
 
-void FlowScriptInterpreter::Lexer(ifstream &input) {
-    errorRow = 0;
-    while(!input.eof()) {
-        string buffer;
+void FlowScriptInterpreter::l_Lexer(ifstream &input) {
+    int currentRow = 0;
+    while(!input.eof() && !e_Flag) {
+		string buffer;
         getline(input, buffer);
         cout << "Lexing line: " << buffer << endl;
-        lexResult.push_back(LexLine(buffer));
-        cout << endl << endl;
-
-        if (errorFlag) {
-            HandleError(errorRow, errorColumn, buffer);
-            return;
-        }
+		rawCodeText.push_back(buffer);
+        lexResult.push_back(l_LexLine(buffer, currentRow));
+        //cout << endl << endl;
 
         //cout << "Line read" << endl;
 
-        errorRow++;
+		currentRow++;
     }
 }
 
-vector<token> FlowScriptInterpreter::LexLine(const string& str) {
+vector<token> FlowScriptInterpreter::l_LexLine(const string& str, int currentRow) {
     vector<token> res;
-    regex delimiter(R"(\s*(->|\w+\b|\W)\s*)");
+    regex delimiter(R"(\s*(->|\w+\b|\W|\n)\s*)");
+
+	int currentToken = 0;
+
+    if (str.empty()) {
+        token temp(EMPTY);
+        res.push_back(temp);
+        return res;
+    }
 
     for (sregex_iterator it(str.begin(), str.end(), delimiter), end; it != end; ++it) {
         string tokenStr = (*it)[1];
-        //cout << "Matching token \"" << tokenStr << "\"." << endl;
-        tokenType type = GetTokenType(tokenStr);
+        //cout << "Matching token \"" << tokenStr << "\" -> ";
+        tokenType type = l_GetTokenType(tokenStr);
+        //cout << "<" << tokenTypeNames[type] << "> " << endl;
 
         if (type == INVALID) {
             cout << endl << endl;
             cout << "INVALID TOKEN \"" + tokenStr + "\"." << endl;
             cout << "LEXER ABORTED." << endl;
             //TODO: ERROR HANDLING
-            errorFlag = true;
+			token error = token(INVALID, (string &) "", currentRow, currentToken);
+			e_HandleError(error, 10); // Lexical error todo: error code
             return res;
         }
 
         //cout << "TOKEN \"" + tokenStr + "\" OF TYPE \"" << tokenTypeNames[type] << "\" READ." << endl;
-        cout << "<" << tokenTypeNames[type] << "> ";
-        res.emplace_back(type, tokenStr);
+        res.emplace_back(type, tokenStr, currentRow, currentToken);
+
+		currentToken++;
     }
 
     return (res);
 }
 
-tokenType FlowScriptInterpreter::GetTokenType(string& str) {
+tokenType FlowScriptInterpreter::l_GetTokenType(string& str) {
     //cout << "Tested: ";
 
     for(const auto& temp : regexMap) {
         //cout << tokenTypeNames[temp.first] <<  " ";
-        try {
-            regex reg(temp.second);
-            if (regex_match(str, reg)) return temp.first;
-        } catch (const regex_error& e) {
-            cout << endl;
-            cerr << "Regex error: " << e.what() << " (code " << e.code() << ")" << endl;
-            // Handle the regex error as needed
-            return INVALID;
-        }
+        regex reg(temp.second);
+        if (regex_match(str, reg)) return temp.first;
     }
     //cout << endl;
 
@@ -93,8 +102,21 @@ tokenType FlowScriptInterpreter::GetTokenType(string& str) {
 }
 
 //todo: move this method to end
-void FlowScriptInterpreter::HandleError(int errorRow, int errorColumn, string& errorLine) {
-    //todo: format text/json file with errors
+void FlowScriptInterpreter::e_HandleError(const token& token, int errorCode) {
+	e_Flag = true;
+	//todo: write error handler
+	cout << "ERROR CODE " << errorCode << endl;
+	cout << "Error occurred on line: " << token.lineIndex + 1 << ", at token: " << token.tokenIndex << endl;
+	cout << "Token is of type: " << tokenTypeNames[token.type] << ", with value: " << token.value << endl;
+	cout << rawCodeText[token.lineIndex] << endl;
+}
+
+void FlowScriptInterpreter::e_HandleError(int lineNum, int errorCode) {
+	e_Flag = true;
+	//todo: write error handler
+	cout << "ERROR CODE " << errorCode << endl;
+	cout << "Error occurred on line: " << lineNum + 1 << endl;
+	cout << rawCodeText[lineNum] << endl;
 }
 
 void FlowScriptInterpreter::printLexResult() {
@@ -108,3 +130,204 @@ void FlowScriptInterpreter::printLexResult() {
         lineNum++;
     }
 }
+
+void FlowScriptInterpreter::i_ResetIterator() {
+	i_braceFlag     = false;
+	i_bracketFlag   = false;
+	i_quoteFlag     = false;
+	i_endFlag       = false;
+	i_curLine       = 0;
+	i_curToken      = 0;
+}
+
+token FlowScriptInterpreter::i_GetCurrent() {
+	return lexResult[i_curLine][i_curToken];
+}
+
+bool FlowScriptInterpreter::i_Increment() {
+	if (i_endFlag) return false;
+
+	// Is current token last in row?
+	if (i_curToken == lexResult[i_curLine].size() - 1) {
+		// Is current row last in file?
+		if (i_curLine == lexResult.size() - 1) {
+			i_endFlag = true;
+			return false;
+		}
+		i_curToken = 0;
+		i_curLine++;
+	} else {
+		i_curToken++;
+	}
+
+	return true;
+}
+
+token FlowScriptInterpreter::i_GetNext() {
+	i_Increment();
+	return i_GetCurrent();
+}
+
+void FlowScriptInterpreter::s_ParseSyntax() {
+    // reset flags and iterator ints
+    i_ResetIterator();
+	token curToken = i_GetCurrent();
+
+	while(!i_endFlag) {
+		vector<token> command;
+
+		while (!binary_search(punctuation.begin(), punctuation.end(), curToken.type) && !i_endFlag) {
+			//cout << curToken << endl;
+			command.push_back(curToken);
+			curToken = i_GetNext();
+		}
+
+		for(auto token : command) {
+			cout << token << " ";
+		}
+		cout << endl;
+
+		//cout << curToken << endl;
+		token endSymbol = curToken;
+
+		switch(endSymbol.type) {
+			case LBRACE:
+				if (i_braceFlag) {
+					e_HandleError(curToken, 1); return; //todo: change error code
+				}
+				i_braceFlag = true;
+				s_ParseGraphDefinition(command, i_curLine);
+				//todo: handle command
+				break;
+			case RBRACE:
+				if (!i_braceFlag) {
+					e_HandleError(curToken, 1); return; //todo: change error code
+				}
+				i_braceFlag = false;
+				break;
+			case SEMICOLON:
+				if (!i_braceFlag) {
+					e_HandleError(curToken, 1); return; //todo: change error code
+				}
+				if (command.size() > 1) s_ParseNodeStatement(command);
+				//todo: handle case where more commands after bracket end
+				break;
+			default:
+				//todo: handle error
+				int a = 3;
+		}
+
+		if(e_Flag) {
+			//e_HandleError(i_curToken, i_curLine, 1); //todo: change error code
+			return;
+		}
+
+		curToken = i_GetNext();
+
+		cout << endl;
+	}
+}
+
+void FlowScriptInterpreter::s_ParseGraphDefinition(vector<token> command, int lineNum) {
+	cout << "Handling graph definition" << endl;
+
+	// Error guard measuring command size
+	if (command.size() != 2) {
+		e_HandleError(lineNum, 1); return; //todo: change error code // Too many/too few arguments for graph definition
+		return;
+	}
+
+	int curIndex = 0;
+	token curToken = command[curIndex];
+
+	if (curToken.type != WORD) {
+		e_HandleError(curToken, 2); // Graph definition error
+		return;
+	}
+
+	graphType = curToken.value; cout << "graph type = " << graphType << endl;
+
+	curIndex++;
+	curToken = command[curIndex];
+
+	if (curToken.type != WORD) {
+		e_HandleError(curToken, 2); // Graph definition error
+		return;
+	}
+
+	graphName = curToken.value; cout << "graph name = " << graphName << endl;
+}
+
+void FlowScriptInterpreter::s_ParseNodeStatement(vector<token> command) {
+	int index = 0;
+	auto commandIter = command.begin();
+
+	cout << "Handling node statement" << endl;
+
+	// Possible stopping point
+	if ((*commandIter).type != WORD) e_HandleError(*commandIter, 2); //todo: change error code
+	if (e_Flag) return;
+	string startNode = (*commandIter).value;
+	commandIter++; index++;
+	if (commandIter == command.end()) {
+		dependencyMap[startNode]; // Add node to map
+		cout << "Added " << startNode << endl;
+		return;
+	}
+
+	if ((*commandIter).type != ARROW) e_HandleError(*commandIter, 2); //todo: change error code
+	if (e_Flag) return;
+	commandIter++; index++;
+
+	// Possible stopping point
+	if ((*commandIter).type != WORD) e_HandleError(*commandIter, 2); //todo: change error code
+	if (e_Flag) return;
+	string endNode = (*commandIter).value;
+	commandIter++; index++;
+	if (commandIter == command.end()) {
+		dependencyMap[endNode].push_back(startNode); // Add node to map todo: doesnt check for duplicates
+		cout << "Added " << startNode << " -> " << endNode << endl;
+		return;
+	}
+
+	if ((*commandIter).type != LBRACKET) e_HandleError(*commandIter, 2); //todo: change error code
+	if (e_Flag) return;
+	commandIter++; index++;
+
+	if ((*commandIter).type != WORD) e_HandleError(*commandIter, 2); //todo: change error code
+	if ((*commandIter).value != "condition") e_HandleError(*commandIter, 2); //todo: change error code
+	if (e_Flag) return;
+	commandIter++; index++;
+
+	if ((*commandIter).type != EQUALS) e_HandleError(*commandIter, 2); //todo: change error code
+	if (e_Flag) return;
+	commandIter++; index++;
+
+	if ((*commandIter).type != QUOTE) e_HandleError(*commandIter, 2); //todo: change error code
+	if (e_Flag) return;
+	commandIter++; index++;
+
+	if ((*commandIter).type != WORD) e_HandleError(*commandIter, 2); //todo: change error code
+	if (e_Flag) return;
+	string condition = (*commandIter).value;
+	commandIter++; index++;
+
+	if ((*commandIter).type != QUOTE) e_HandleError(*commandIter, 2); //todo: change error code
+	if (e_Flag) return;
+	commandIter++; index++;
+
+	if ((*commandIter).type != RBRACKET) e_HandleError(*commandIter, 2); //todo: change error code
+	if (e_Flag) return;
+	commandIter++; index++;
+
+	if (commandIter != command.end()) e_HandleError(*commandIter, 2); //todo: change error code
+	if (e_Flag) return;
+
+	cout << "Added " << startNode << " -> " << endNode << " | Condition: " << condition << endl;
+}
+
+
+
+
+
+
--- Code/FlowScriptInterpreter.h
+++ Code/FlowScriptInterpreter.h
@@ -9,86 +9,146 @@
 #include <fstream>
 #include <utility>
 #include <regex>
+#include <cctype>
 
 enum tokenType {
-    SEMICOLON,
-    WORD,
-    ARROW,
-    LBRACE,
-    RBRACE,
-    LBRACKET,
-    RBRACKET,
-    EQUALS,
-    QUOTE,
-    INVALID
+	SEMICOLON,
+	WORD,
+	ARROW,
+	LBRACE,
+	RBRACE,
+	LBRACKET,
+	RBRACKET,
+	EQUALS,
+	QUOTE,
+	INVALID,
+	EMPTY
 };
 
 static const char* tokenTypeNames[] = {
-    "SEMICOLON",
-    "WORD",
-    "ARROW",
-    "LBRACE",
-    "RBRACE",
-    "LBRACKET",
-    "RBRACKET",
-    "EQUALS",
-    "QUOTE",
-    "INVALID"
+	"SEMICOLON",
+	"WORD",
+	"ARROW",
+	"LBRACE",
+	"RBRACE",
+	"LBRACKET",
+	"RBRACKET",
+	"EQUALS",
+	"QUOTE",
+	"INVALID",
+	"EMPTY"
+};
+
+static const vector<tokenType> punctuation = {
+	SEMICOLON,
+	LBRACE,
+	RBRACE,
+	EMPTY
 };
 
 static const map<tokenType, string> regexMap = {
-        {SEMICOLON, R"(;)"},
-        {ARROW,     R"(->)"},
-        {LBRACE,    R"(\{)"},
-        {RBRACE,    R"(\})"},
-        {LBRACKET,  R"(\[)"},
-        {RBRACKET,  R"(\])"},
-        {EQUALS,    R"(=)"},
-        {QUOTE,     R"(")"},
-        {WORD,      R"(\w+)"},
-        {INVALID,   R"(.*)"}
+	{SEMICOLON, R"(;)"},
+	{ARROW,     R"(->)"},
+	{LBRACE,    R"(\{)"},
+	{RBRACE,    R"(\})"},
+	{LBRACKET,  R"(\[)"},
+	{RBRACKET,  R"(\])"},
+	{EQUALS,    R"(=)"},
+	{QUOTE,     R"(")"},
+	{WORD,      R"(\w+)"},
+	{INVALID,   R"(.*)"}
+};
+
+static const vector<string> graphTypes = {
+	"digraph",
+	"graph"
+};
+
+static const unordered_map<int, string> eMessageMap = {
+		{1, R"(;)"},
+		{2, R"(;)"},
+		{3,     R"(->)"},
+		{4,    R"(\{)"},
+		{5,    R"(\})"},
+		{6,  R"(\[)"},
+		{7,  R"(\])"},
+		{8,    R"(=)"},
+		{9,     R"(")"},
+		{10,      R"(\w+)"},
+		{11,   R"(.*)"}
 };
 
 struct token {
 public:
-    token() = default;
-    token(tokenType type, string& value) : type(type), value(value){}
+	token() = default;
+	token(tokenType type) : type(type){}
+	token(tokenType type, string& value) : type(type), value(value){}
+	token(tokenType type, string& value, int lineIndex, int tokenIndex)
+		: type(type), value(value), lineIndex(lineIndex), tokenIndex(tokenIndex){}
 
-    tokenType type = INVALID;
-    string value;
+	tokenType type = INVALID;
+	string value;
 
-    friend ostream& operator<<(ostream& os, const token& token);
+	int lineIndex = -1;
+	int tokenIndex = -1;
+
+	friend ostream& operator<<(ostream& os, const token& token);
+	bool operator==(tokenType other) const;
 };
 
 class FlowScriptInterpreter {
 public:
-    FlowScriptInterpreter() = default;
-    FlowScriptInterpreter(const string& relativePath);
-    FlowScriptInterpreter(ifstream &input);
+	FlowScriptInterpreter() = default;
+	FlowScriptInterpreter(const string& relativePath);
+	FlowScriptInterpreter(ifstream &input);
 
-    void Interpret(ifstream &input);
+	//main driver function
+	void Interpret(ifstream &input);
 
-    void Lexer(ifstream &input);
-    vector<token> LexLine(const string& str);
-    static tokenType GetTokenType(string &str);
+	//l_Lexer functions todo: maybe move to private???
+	void l_Lexer(ifstream &input);
+	vector<token> l_LexLine(const string& str, int currentRow);
+	static tokenType l_GetTokenType(string &str);
 
-    void printLexResult();
+	//Syntax parser todo: maybe move to private
+	void s_ParseSyntax();
+	void s_ParseGraphDefinition(vector<token> command, int lineNum); //todo: rename to something better
+	void s_ParseNodeStatement(vector<token> command); //todo: ditto
 
-    //first = node, second = node's dependencies
-    unordered_map<string, vector<string>> dependencyMap;
+	//printer functions
+	void printLexResult();
+	bool isGood() { return e_Flag; }
 
-    //first = node name, second = node job type
-    unordered_map<string, string> jobMap;
-
-    //first = value, second = tokenType type
-    vector<vector<token>> lexResult;
+	string graphName;
+	string graphType;
+	vector<string> rawCodeText;
+	unordered_map<string, vector<string>> dependencyMap; // <node, node dependencies>
+	unordered_map<string, string> jobMap;               // <node name, node job type>
+	vector<vector<token>> lexResult;
 
 private:
-    void HandleError(int errorRow, int errorColumn, string& errorLine);
+	//for error handling
+	void e_HandleError(const token& token, int errorCode);
+	void e_HandleError(int lineNum, int errorCode); // for lexical
 
-    bool errorFlag = false;
-    int errorRow = 0;
-    int errorColumn = 0;
+	bool e_Flag      	= false;
+	int e_Row       	= -1;
+	int e_Token    		= -1;
+	string e_Value;
+
+	//for lex iterator
+	void i_ResetIterator();
+	bool i_Increment();
+	token i_GetCurrent();
+	token i_GetNext();
+
+	bool i_braceFlag    = false;
+	bool i_bracketFlag  = false;
+	bool i_quoteFlag    = false;
+	bool i_endFlag      = false;
+
+	int  i_curLine      = 0;
+	int  i_curToken     = 0;
 };
 
 
